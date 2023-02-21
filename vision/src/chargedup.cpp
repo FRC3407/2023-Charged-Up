feed61dcd9f149f39a7356562e80fc1e95f2cf87
Sam Richter <hoodie888@outlook.com>
2023-02-20 22:45:05 -0600

    more vision

--- vision/src/chargedup.cpp
+++ vision/src/chargedup.cpp
@@ -6,10 +6,12 @@
 #include <vector>
 #include <array>
 
-// #include <opencv2/aruco.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/aruco.hpp>
 
 #include <fmt/format.h>
 
+#include <cscore_cv.h>
 #include <cscore_cpp.h>
 // #include <wpi/sendable/Sendable.h>
 
@@ -29,9 +31,8 @@ struct {
 	std::chrono::system_clock::time_point start_time;
 
 	cs::VideoMode camera_vmode = {
-		cs::VideoMode::PixelFormat::kMJPEG, 640, 480, 30};
+		cs::VideoMode::kMJPEG, 640, 480, 30};
 	CS_Source camera_src_handle;
-	CS_Sink camera_raw_handle;
 	CS_Sink output_stream;
 
 } _global;
@@ -46,10 +47,12 @@ int main(int argc, char** argv) {
 	}
 	// initialize nt
 
-	cs::SetSinkSource(
-		_global.output_stream,
-		_global.camera_src_handle,
-		&status);
+	cs::SetCameraExposureManual(
+		_global.camera_src_handle, 20, &status);
+	// cs::SetSinkSource(
+	// 	_global.output_stream,
+	// 	_global.camera_src_handle,
+	// 	&status);
 
 	std::thread		// start the processing threads
 		april_runner{_apriltag},
@@ -64,6 +67,9 @@ int main(int argc, char** argv) {
 			_global.s_program_enable = false;
 		};
 		sigaction((unsigned int)SigHandle::Sigs::INT, &_action, nullptr);
+		sigaction((unsigned int)SigHandle::Sigs::QUIT, &_action, nullptr);
+		sigaction((unsigned int)SigHandle::Sigs::ILL, &_action, nullptr);
+		sigaction((unsigned int)SigHandle::Sigs::SEGV, &_action, nullptr);
 	}
 
 	for(;_global.s_program_enable;) {
@@ -83,7 +89,7 @@ int main(int argc, char** argv) {
 
 	// deinit
 	cs::ReleaseSource(_global.camera_src_handle, &status);
-	cs::ReleaseSink(_global.camera_raw_handle, &status);
+	cs::ReleaseSink(_global.output_stream, &status);
 
 	std::cout << "Runtime: " <<
 		std::chrono::duration<double>(
@@ -99,8 +105,7 @@ bool init() {
 	int status = 0;
 	std::vector<cs::UsbCameraInfo> _cameras =
 		cs::EnumerateUsbCameras(&status);
-	_global.output_stream =
-		cs::CreateMjpegServer("Test Stream", "", 1181, &status);
+	_global.output_stream = cs::CreateMjpegServer("Test Stream", "", 1181, &status);
 	cs::SetProperty(
 		cs::GetSinkProperty(_global.output_stream, "width", &status),
 		_global.camera_vmode.width, &status);
@@ -112,18 +117,16 @@ bool init() {
 		_global.camera_vmode.fps, &status);
 	for(unsigned int i = 0; i < _cameras.size(); i++) {
 		if(strncmp(_cameras[i].name.c_str(), "bcm", 3) != 0) {
+			std::cout << "Camera Detected: " << _cameras[i].name << std::endl;
+
 			_global.camera_src_handle = cs::CreateUsbCameraPath(
 				"Test Camera", _cameras[i].path, &status);
+			cs::SetSourceConnectionStrategy(
+				_global.camera_src_handle,
+				CS_CONNECTION_KEEP_OPEN, &status);
 			cs::SetSourceVideoMode(
 				_global.camera_src_handle,
-				_global.camera_vmode,
-				&status);
-			_global.camera_raw_handle =
-				cs::CreateCvSink("Test Camera [CV]", &status);
-			cs::SetSinkSource(
-				_global.camera_raw_handle,
-				_global.camera_src_handle,
-				&status);
+				_global.camera_vmode, &status);
 
 			// std::vector<cs::VideoMode> _videomodes =
 			// 	cs::EnumerateSourceVideoModes(_global.camera_src_handle, &status);
@@ -143,24 +146,47 @@ bool init() {
 void _apriltag() {
 
 	int status = 0;
-	CS_Source cv_out =
-		cs::CreateCvSource("apriltag_cv_out", _global.camera_vmode, &status);
+	CS_Sink cv_in = cs::CreateCvSink("apriltag_cv_in", &status);
+	CS_Source cv_out = cs::CreateCvSource("apriltag_cv_out", _global.camera_vmode, &status);
+	cs::SetSinkSource(cv_in, _global.camera_src_handle, &status);
+	cs::SetSinkSource(_global.output_stream, cv_out, &status);
+
+	cv::Mat frame;
+	std::chrono::high_resolution_clock::time_point tp = std::chrono::high_resolution_clock::now();
 
 	for(;_global.s_program_enable;) {
 
+		uint64_t dt = cs::GrabSinkFrame(cv_in, frame, &status);
+		std::chrono::high_resolution_clock::time_point t = std::chrono::high_resolution_clock::now();
+		float fps = 1.f / std::chrono::duration<float>(t - tp).count();
+		tp = t;
+		cv::putText(
+			frame, std::to_string(fps),
+			cv::Point(5, 20), cv::FONT_HERSHEY_DUPLEX,
+			0.65, cv::Scalar(0, 255, 0), 1, cv::LINE_AA
+		);
+		cs::PutSourceFrame(cv_out, frame, &status);
+
 		std::this_thread::sleep_for(20ms);
 	}
+
+	cs::ReleaseSink(cv_in, &status);
+	cs::ReleaseSource(cv_out, &status);
 
 }
 void _retroreflective() {
 
 	int status = 0;
-	CS_Source cv_out =
-		cs::CreateCvSource("retroref_cv_out", _global.camera_vmode, &status);
+	CS_Sink cv_in = cs::CreateCvSink("retroref_cv_in", &status);
+	CS_Source cv_out = cs::CreateCvSource("retroref_cv_out", _global.camera_vmode, &status);
+	cs::SetSinkSource(cv_in, _global.camera_src_handle, &status);
 
 	for(;_global.s_program_enable;) {
 
 		std::this_thread::sleep_for(20ms);
 	}
+
+	cs::ReleaseSink(cv_in, &status);
+	cs::ReleaseSource(cv_out, &status);
 
 }
